e957dbd8e0be928e1cfa4f9fededc19ba5b9bfa0
VÍCTOR SILVEIRA RODRIGUES <victor_rodrigues2@estudante.sesisenai.org.br>
2024-08-19 12:36:32 -0300

    alterações

--- app/lista-tarefa/Index.jsx
+++ app/lista-tarefa/Index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native"
 
 const styles = StyleSheet.create({
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
 
 })
 
-const lista = [
+const tarefa = [
     { id: 1, descricao: 'Comer torrada antes de dormir', feito: false },
     { id: 2, descricao: 'Jogar jogo', feito: false },
     { id: 3, descricao: 'Estudar', feito: true },
@@ -24,14 +24,23 @@ const lista = [
 ]
 
 export default lista_tarefa = () => {
+
+    const[tasks, settask] = useState(tarefa)
+
+   const  li = function (item){
+    console.log(item)
+    var procurar = tasks.find((task)=>item.id == task.id)
+    procurar.feito = !procurar.feito
+    settask([...tasks])
+   }
+
     return <View style={styles.container}>
-        
-            <Text style={styles.title}></Text>
-    
-        <FlatList
-            data={lista}
-            renderItem={({item}) => <View style={styles.container}>
-                <Text style={[item.feito ? styles.tarefa_concluida : styles.tarefa_nao_feita]}>
+        <Text style={styles.title}></Text>
+
+            <FlatList
+            data={tarefa}
+            renderItem={({ item }) => <View style={styles.container}>
+                <Text onPress={()=>li(item)} style={[item.feito ? styles.tarefa_concluida : styles.tarefa_nao_feita]}>
                     {item.descricao}
                 </Text>
             </View>}>
