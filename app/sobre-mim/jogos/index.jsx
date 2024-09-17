08f88f00a9b183bf9f98331a9a22c20d184d9b88
VÍCTOR SILVEIRA RODRIGUES <victor_rodrigues2@estudante.sesisenai.org.br>
2024-09-17 12:33:30 -0300

    alteracoes

--- app/sobre-mim/jogos/index.jsx
+++ app/sobre-mim/jogos/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, FlatList, Pressable, ScrollView, } from "react-native"
+import { View, Text, StyleSheet, FlatList, Pressable, ScrollView,SafeAreaView } from "react-native"
 import {Link} from "expo-router"
 import Navbar from "../../../components/Navbar";
 import GamesAboutMe from "../../../components/Games-movies-AboutMe";
-import { SafeAreaView } from "react-native-safe-area-context";
+
 
 const style = StyleSheet.create({
     
