971491f6abdb74388958ab7a02fb634cdce0a44e
victor03f <victorsilveira387@gmail.com>
2024-10-09 20:06:55 -0300

    alteracoes

--- app/camera/index.jsx
+++ app/camera/index.jsx
@@ -7,6 +7,9 @@ import { Linking } from "react-native";
 
 
 
+
+
+
 export default camera = () => {
   const [permissions, requestPermissions] = useCameraPermissions();
   const [foto, setFoto] = useState(null);
@@ -16,15 +19,16 @@ export default camera = () => {
   const [ladoCamera, SetladoCamera] = useState("back");
   const [permissaoSalvar, pedirpermissaoSalvar] = MediaLibrary.usePermissions();
 
+
   if (!permissions) {
     return <View></View>;
   }
+
 
   if (!permissions.granted) {
     return (
       <View style={style.container}>
         <Text style={style.TextPermission}>
-          {" "}
           Voce precisa da permissao para utilizar a camera
         </Text>
         <Button title="pedir permissão" onPress={requestPermissions} />
@@ -32,7 +36,8 @@ export default camera = () => {
     );
   }
 
-  const tirarfoto = async () => {
+
+  const tirarFoto = async () => {
     const fotoBase64 = await cameraRef.current?.takePictureAsync({
       quality: 0.5,
       base64: true,
@@ -44,8 +49,9 @@ export default camera = () => {
     SetladoCamera(ladoCamera == "back" ? "front" : "back");
   };
 
+
   const salvaFoto = async () => {
-    if (!permissaoSalvar.status == "granted") {
+    if (permissaoSalvar.status !== "granted") {
       await pedirpermissaoSalvar();
     }
     MediaLibrary.saveToLibraryAsync(foto.uri);
@@ -57,101 +63,108 @@ export default camera = () => {
     setScanned(false);
 };
 
-  return (
-    <View style={style.container}>
-      {foto ?(
-        <SafeAreaView style={style.container}>
 
-          <Image style={style.image} source={{ uri: foto.uri }} />
-          <View style={style.ViewButton}>
+return (
+  <View style={style.container}>
+    {foto ? (
+      <SafeAreaView style={style.Viewcontainer}>
+        <View style={style.ViewFoto}>
 
-            <Button style={style.ButtonFoto} title="Descartar imagem" onPress={() => setFoto(null)} />
-            <Button title="Salvar Foto" onPress={salvaFoto} />
-          </View>
-          
-        </SafeAreaView>
-        
-        ):
-        
-        <View>
-          </View>
-}
- {scanner ? (
+       
+        <Image style={style.image} source={{ uri: foto.uri }} />
+        <View style={style.viewButton}>
+              <Pressable title="Salvar Foto" onPress={salvaFoto}>
+                <Image style={style.icon} source={require('../image/salvar.png')} />
+              </Pressable>
+              <Pressable title="Descartar imagem" onPress={() => setFoto(null)}>
+                <Image style={style.icon} source={require('../image/salvar.png')} />
+              </Pressable>
+              <Pressable title="Salvar Foto" onPress={salvaFoto}>
+                <Image style={style.icon} source={require('../image/salvar.png')} />
+              </Pressable>
+              <Pressable onPress={tirarFoto}>
+                <Image style={style.icon} source={require('../image/deletar.png')} />
+              </Pressable>
+           
+        </View>
+        </View>
+      </SafeAreaView>
+    ) : (
+      <View>
+        {scanner ? (
           <View style={style.camera}>
-              <BarCodeScanner
-                  onBarCodeScanned={scanned ? undefined : readQr}
-                  style={StyleSheet.absoluteFillObject}
-              />
-              <View style={style.cambtn}>
-                  <Pressable style={style.button} onPress={() => setScanner(!scanner)}>
-                      
-                  </Pressable>
-              </View>
-        </View> 
- ):(
-        <CameraView style={style.camera} ref={cameraRef} facing={ladoCamera}
-        >
-          <View style={style.cameraContainer}>
-            <Pressable onPress={tirarfoto}>
-              <Image
-                style={style.fotoDePerfil}
-                source={require('../image/camera.png')}
-
-              />
-            </Pressable>
-            <Pressable style={style.button} onPress={() => setScanner(!scanner)}>
-                      <Image
-                          style={style.fotoDePerfil}
-                          source={require('../image/qr-code.png')}
-                      />
-                  </Pressable>
-
-            <Pressable onPress={inverterLadoCamera}>
-              <Image
-                style={style.fotoDePerfil}
-                source={require('../image/trocaLado.png')}
-
-              />
-            </Pressable>
+            <BarCodeScanner
+              onBarCodeScanned={scanned ? undefined : readQr}
+              style={StyleSheet.absoluteFillObject}
+            />
+            <View style={style.cambtn}>
+              <Pressable style={style.button} onPress={() => setScanner(!scanner)}>
+                
+              </Pressable>
+            </View>
           </View>
-        </CameraView>
-      )}
-    </View>
-    
-  );
-};
+        ) : (
+          <CameraView style={style.camera} ref={cameraRef} facing={ladoCamera}>
+            <View style={style.cameraContainer}>
+              <Pressable onPress={tirarFoto}>
+                <Image style={style.icon} source={require('../image/camera.png')} />
+              </Pressable>
+              
+              <Pressable style={style.button} onPress={() => setScanner(!scanner)}>
+                <Image style={style.icon} source={require('../image/qr-code.png')} />
+              </Pressable>
+              <Pressable onPress={inverterLadoCamera}>
+                <Image style={style.icon} source={require('../image/trocaLado.png')} />
+              </Pressable>
+            </View>
+          </CameraView>
+        )}
+      </View>
+    )}
+  </View>
+);
+}
+
+
 
 const style = StyleSheet.create({
-  container: {
+
+container:{
+  flex: 1
+},
+  Viewcontainer:{
     flex: 1,
-    justifyContent: "center",
+   
   },
-  TextPermission: {
-    TextAlign: "center",
-  },
-  camera: {
+  ViewFoto:{
     flex: 1,
+    
   },
-  image: {
+  image:{
     width: "100%",
     height: "100%",
   },
-  ButtonFoto: {
-    flex: 1,
+  camera:{
+    width: "100%",
+    height: "100%",
   },
-  ViewButton: {
-    justifyContent: 'flex-end'
-  },
-
-  fotoDePerfil: {
-    width: 60,
-    height: 60,
-  },
-  cameraContainer: {
-    flex: 1,
-    alignItems: 'flex-end',
-    justifyContent: 'space-around',
-    flexDirection: 'row'
-
-  }
+    cameraContainer:{
+      flex: 1,
+      alignItems: 'flex-end',
+      flexDirection: 'row',
+      justifyContent: 'space-around'
+    },
+    icon:{
+      width: 55,
+      height: 55
+    },
+    viewButton:{
+      flex: 1,
+      alignItems: 'flex-end',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly'
+    }
 });
+
+
+
